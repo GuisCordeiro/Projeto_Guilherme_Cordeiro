7e007be9d8bc0352b6ae77a75bcc22ab3a6c5776
guiscordeiro <guilhermedossantoscordeiro13@gmail.com>
2023-10-08 13:28:25 -0300

    Criação da classe C

---
+++ classeC.py
@@ -0,0 +1,26 @@
+class C:
+    def __init__(self):
+        self.C1 = ""  # Atributo String C1
+        self.C2 = 0  # Atributo int C2
+
+    # Getter e Setter para C1
+    def get_C1(self):
+        return self.C1
+
+    def set_C1(self, valor):
+        self.C1 = valor
+
+    # Getter e Setter para C2
+    def get_C2(self):
+        return self.C2
+
+    def set_C2(self, valor):
+        self.C2 = valor
+
+    # Método MC1
+    def MC1(self):
+        print("Método MC1 da classe C")
+
+    # Método MC2
+    def MC2(self):
+        print("Método MC2 da classe C")
