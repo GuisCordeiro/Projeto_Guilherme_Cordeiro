ebe859c05bbbdf01a432ee63e8d19ae7cee92ead
guiscordeiro <guilhermedossantoscordeiro13@gmail.com>
2023-10-08 19:47:29 -0300

    criando a classe D

---
+++ classeD.py
@@ -0,0 +1,16 @@
+class D:
+    def __init__(self, D1, D2):
+        self.D1 = D1  # Atributo D1 (pode ser de qualquer tipo de dados)
+        self.D2 = D2  # Atributo D2 (pode ser de qualquer tipo de dados)
+
+    # Método MD1 com parâmetro de entrada
+    def MD1(self, parametro):
+        # Assinatura: def MD1(self, parametro)
+        # Tipo de dado do parâmetro: Qualquer tipo de dado
+        print(f"Método MD1 da classe D com parâmetro: {parametro}")
+
+    # Método MD2 sem parâmetros
+    def MD2(self):
+        # Assinatura: def MD2(self)
+        print("Método MD2 da classe D")
+
